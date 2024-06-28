c42f3de2848545ab1af707612dfe7fed1b754521
sparklyphd <37736166+sparklyphd@users.noreply.github.com>
2024-06-28 13:33:00 -0400

    Update Recipe_Managing_privileges_in_Unity_Catalog.py

--- Chapter03/Recipe_Managing_privileges_in_Unity_Catalog.py
+++ Chapter03/Recipe_Managing_privileges_in_Unity_Catalog.py
@@ -1,3 +1,11 @@
-# Managing privileges in Unity Catalog
+#In Unity Catalog, privileges are managed through SQL commands. To give a user or group privileges
+#on an object, use the GRANT statement:
 
-# Placeholder for actual content
+GRANT <privilege-name> ON <object-name> TO <user-or-group>
+
+#For example, if you have a table named Customers in the default schema of the sales catalog
+#and you want to grant read privileges on the table to the sales_analysts group, you can use the
+#following command:
+
+  GRANT SELECT ON sales.default.Customers TO sales_analysts
+
